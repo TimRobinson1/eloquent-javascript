6aa0a91c3c74e01c10bff10272e2ff53d151df48
Timothy Robinson <timmyrobby@gmail.com>
2017-04-04 09:12:54 +0100

    Adds first exercise

---
+++ chap01/ex01.js
@@ -0,0 +1,24 @@
+// Basic logging to the console with various input types.
+
+// This command is used to display information in the console.
+console.log("Hello world")
+
+// A new line character is written as a '\n'
+console.log("First line\nSecond line")
+
+// Strings can also be broken up and concatenated.
+console.log("This " + "is " + "one " + "sentence")
+
+// Comments like these are marked with double forward slashes.
+// It can also be used for integers or equations. Eg:
+console.log(42)
+
+console.log(20 / 5)
+
+console.log( (10 * 5) / 2)
+
+//Using the typeof operator, we can check what type of value was given
+console.log(typeof 42)
+// This will produce "number"
+console.log(typeof "Hello")
+// This will produce "String"
